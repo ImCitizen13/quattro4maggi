0009a583c20bf7b763a981f5f6507ced82b9975b
ImCitizen13 <meltohamy@protonmail.com>
2026-04-24 20:31:56 +0400

    Enhance HomeScreen layout and demo entries; integrate new Lottie animations for Ripple Effect, Scale Flip Card, Live Border Card, and Liquid Metal. Update demo structure to include tags and Lottie links, and replace previous demo components with a unified DemoCardView for improved presentation.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,28 +1,43 @@
-import { Href, Link } from "expo-router";
-import { FlatList, Pressable, StyleSheet, Text } from "react-native";
+import DemoCardView, { Demo } from "@/components/ui/DemoCardView";
+import { FlatList, StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-type Demo = {
-  name: string;
-  href: Href;
-};
-
 const demos: Demo[] = [
-  { name: "Ripple Effect", href: "/ripple-effect" as const },
-  { name: "Scale Flip Card", href: "/scale-flip-card" as const },
-  { name: "Live Border Card", href: "/live-border-card" as const },
-  { name: "Liquid Metal", href: "/liquid-metal" as const },
+  {
+    name: "Ripple Effect",
+    href: "/ripple-effect" as const,
+    tags: [
+      { name: "skia", color: "green" },
+      { name: "reanimated", color: "#6a539a" },
+    ],
+    lottieLink: "ripple.json"
+  },
+  {
+    name: "Scale Flip Card",
+    href: "/scale-flip-card" as const,
+    tags: [{ name: "reanimated", color: "#6a539a" }],
+    lottieLink: "scale-flip-card.json"
+  },
+  {
+    name: "Live Border Card",
+    href: "/live-border-card" as const,
+    tags: [
+      { name: "skia", color: "green" },
+      { name: "reanimated", color: "#6a539a" },
+    ],
+    lottieLink: "live-border.json"
+  },
+  {
+    name: "Liquid Metal",
+    href: "/liquid-metal" as const,
+    tags: [
+      { name: "skia", color: "green" },
+      { name: "reanimated", color: "#6a539a" },
+    ],
+    lottieLink: "liquid-metal.json"
+  },
 ];
 
-const ItemCellView = ({ index, demo }: { index: number; demo: Demo }) => {
-  return (
-    <Link key={index} href={demo.href} asChild>
-      <Pressable style={styles.card}>
-        <Text style={styles.cardTitle}>{demo.name}</Text>
-      </Pressable>
-    </Link>
-  );
-};
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
@@ -30,13 +45,15 @@ export default function HomeScreen() {
       <FlatList
         style={{ flex: 1 }}
         data={demos}
-        renderItem={({ index, item }) => {
-          return <ItemCellView index={index} demo={item} />;
+        numColumns={2}
+        columnWrapperStyle={{
+          justifyContent: "space-between"
         }}
-        
+        renderItem={({ index, item }) => (
+          <DemoCardView index={index} demo={item} />
+        )}
         contentContainerStyle={styles.grid}
       />
-
     </SafeAreaView>
   );
 }
@@ -44,7 +61,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 14
+    padding: 14,
   },
   title: {
     fontSize: 28,
@@ -54,15 +71,5 @@ const styles = StyleSheet.create({
   },
   grid: {
     gap: 12,
-  },
-  card: {
-    backgroundColor: "#1a1a1a",
-    borderRadius: 12,
-    padding: 20,
-  },
-  cardTitle: {
-    color: "#fff",
-    fontSize: 18,
-    fontWeight: "600",
   },
 });
---
+++ src/components/ui/DemoCardView.tsx
@@ -0,0 +1,113 @@
+import { Image } from "expo-image";
+import { Href, Link } from "expo-router";
+import LottieView from "lottie-react-native";
+import { PressableScale } from "pressto";
+import React, { useRef } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const LOTTIE_ASSETS = {
+  "liquid-metal.json": require("../../../assets/lottie/liquid-metal_dark.json"),
+  "live-border.json": require("../../../assets/lottie/live-border_dark.json"),
+  "ripple.json": require("../../../assets/lottie/ripple_dark.json"),
+  "scale-flip-card.json": require("../../../assets/lottie/scale-flip-card_dark.json"),
+} as const;
+const BACKGROUND_COLOR = "#1a1a1a";
+export type Tag = {
+  name: string;
+  color: string;
+};
+
+export type Demo = {
+  name: string;
+  href: Href;
+  tags: Array<Tag>;
+  lottieLink?: keyof typeof LOTTIE_ASSETS;
+};
+
+export default function DemoCardView({
+  index,
+  demo,
+}: {
+  index: number;
+  demo: Demo;
+}) {
+  const animation = useRef<LottieView>(null);
+  const lottie = demo.lottieLink ? LOTTIE_ASSETS[demo.lottieLink] : null;
+  return (
+    <Link
+      style={{
+        backgroundColor: "transparent",
+        width: 180,
+        justifyContent: "center",
+      }}
+      key={index}
+      href={demo.href}
+      asChild
+    >
+      <PressableScale
+        style={styles.card}
+        onPressIn={() => {
+          animation.current?.play();
+        }}
+        onPressOut={() => {
+          animation.current?.pause();
+        }}
+      >
+        <Text style={styles.cardTitle}>{demo.name}</Text>
+        {lottie && (
+          <LottieView
+            ref={animation}
+            style={{
+              width: 100,
+              height: 100,
+              backgroundColor: BACKGROUND_COLOR,
+            }}
+            source={lottie}
+          />
+        )}
+        <View
+          style={{ flexDirection: "row", gap: 12, justifyContent: "center" }}
+        >
+          {demo.tags.map((tag, idx) => {
+            return (
+              <View key={idx}>
+                <Image
+                  source={
+                    tag.name == "skia"
+                      ? require("../../../assets/tags/skia.png")
+                      : require("../../../assets/tags/reanimated.png")
+                  }
+                  contentFit="cover"
+                  style={{
+                    width: 50,
+                    height: 25,
+                    borderWidth: 1,
+                    borderColor: "rgb(66, 66, 66)",
+                    borderRadius: 4,
+                  }}
+                />
+              </View>
+            );
+          })}
+        </View>
+      </PressableScale>
+    </Link>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: BACKGROUND_COLOR,
+    borderRadius: 12,
+    gap: 12,
+    padding: 20,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  cardTitle: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "600",
+  },
+});
